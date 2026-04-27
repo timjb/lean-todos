b52996d5d7cbd83be30887f98c959d9b788415fc
Tim J. Baumann <tim@timbaumann.info>
2026-04-27 22:06:11 +0200

    clean up

--- Todos/EnvExtension.lean
+++ Todos/EnvExtension.lean
@@ -3,19 +3,6 @@ import Todos.Types
 
 open Lean
 
-/-
-initialize todosRef : IO.Ref (Array TodoItem) ← IO.mkRef #[]
-
-def addTodo (item : TodoItem) : IO Unit := do
-  todosRef.modify $ fun items =>
-    items
-      |>.filter (fun i => i.name != item.name)
-      |>.push item
-
-def getTodos : IO (Array TodoItem) := do
-  todosRef.get
--/
-
 initialize todosExtension :
     SimplePersistentEnvExtension TodoItem (List TodoItem) ←
   registerSimplePersistentEnvExtension {
@@ -28,8 +15,3 @@ def addTodo {m} [Monad m] [MonadEnv m] (item : TodoItem) : m Unit := do
 
 def getTodos {m} [Monad m] [MonadEnv m] : m (List TodoItem) := do
   pure (todosExtension.getState (← getEnv)).reverse
-
-  -- todosRef.modify $ fun items =>
-  --   items
-  --     |>.filter (fun i => i.name != item.name)
-  --     |>.push item
